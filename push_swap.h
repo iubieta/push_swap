153faef0abc80caa5381340cd897ce24c0038907
iubieta <ikerp.ubieta@gmail.com>
2024-03-10 20:15:00 +0100

    Norminette pasada

--- push_swap.h
+++ push_swap.h
@@ -17,20 +17,22 @@
 # include <limits.h>
 # include "./libft/libft.h"
 
-typedef struct int_list {
-	int	content;
-	struct int_list *next;
-} int_list;
+typedef struct s_list
+{
+	int				content;
+	struct s_list	*next;
+}					t_list;
 
-int 		ft_checkErrors(int argc, char **argv, int_list **stack);
-char		*ft_nextElement(char *element);
-int			ft_checkPush(char *element, int_list **stack);
-int 		ft_checkElement(char *element);
+int			ft_check_errors(int argc, char **argv, t_list **stack);
+char		*ft_first_element(char *argument);
+char		*ft_next_element(char *element);
+int			ft_check_push(char *element, t_list **stack);
+int			ft_check_element(char *element);
 long		ft_atol(const char *s);
-int_list 	*ft_lst_newNode(int content);
-void		ft_lst_addFront(int_list **list, int_list *node);
-void		ft_printList(int_list *head);
-void		ft_freeList(int_list **list);
-int			ft_existsInList(int number, int_list *head);
+t_list		*ft_lst_new_node(int content);
+void		ft_lst_add_front(t_list **list, t_list *node);
+void		ft_prt_list(t_list *head);
+void		ft_free_list(t_list **list);
+int			ft_exists_in_list(int number, t_list *head);
 
-# endif
+#endif
